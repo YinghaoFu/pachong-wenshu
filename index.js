b45d2f92bb2228cf6abe7e80491121e91e3ee9ae
fuyinghao <fuyinghao@zhiwen.me>
2020-03-24 01:50:53 +0800

    20200324

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const puppeteer = require('puppeteer');
   const browser = await puppeteer.launch({
     // headless: false, // 开启界面
     // defaultViewport: null,
-    // slowMo: 150,
+    // slowMo: 80,
   });
   const page = await browser.newPage();
   // 文书网
@@ -17,24 +17,53 @@ const puppeteer = require('puppeteer');
   await page.waitForSelector('#_view_1540966814000 > div > div.search-wrapper.clearfix > div.advenced-search');
   // 高级检索
   await page.click('#_view_1540966814000 > div > div.search-wrapper.clearfix > div.advenced-search');
+  const searchText = '村规民约';
   // 全文检索关键字
-  await page.type('#qbValue', '村民委员会');
+  await page.type('#qbValue', searchText);
   // 全文检索类型
   await page.click('#qbType');
-  // 当事人段
-  await page.click('#qwTypeUl > li:nth-child(3)');
-  // 案件类型
-  await page.click('#selectCon_other_ajlx');
+  // 理由
+  await page.click('#qwTypeUl > li:nth-child(6)');
+  // // 案件类型
+  // await page.click('#selectCon_other_ajlx');
   // 民事案件 
   // await page.click('#gjjs_ajlx > li:nth-child(4)');
-  // 行政案件
-  await page.click('#gjjs_ajlx > li:nth-child(5)');
-  // 文书类型
-  await page.click('#_view_1540966814000 > div > div.advencedWrapper > div.inputWrapper.clearfix > div:nth-child(9) > div > div > div');
-  // 判决书
-  await page.click('#gjjs_wslx > li:nth-child(3)');
+  // // 行政案件
+  // await page.click('#gjjs_ajlx > li:nth-child(5)');
+  // // 文书类型
+  // await page.click('#_view_1540966814000 > div > div.advencedWrapper > div.inputWrapper.clearfix > div:nth-child(9) > div > div > div');
+  // // 判决书
+  // await page.click('#gjjs_wslx > li:nth-child(3)');
   // 裁决书
   // await page.click('#gjjs_wslx > li.on');
+  // 年份开始（2017-01-01）
+  await page.type('#cprqStart', '2017-01-01');
+  // 年份结束（2020-12-31）
+  await page.type('#cprqEnd', '2020-12-31');
+  // 当事人
+  await page.type('#s17', '村民委员会');
+
+  // 检索
+  await page.click('#searchBtn');
+  await page.waitForSelector('#_view_1545184311000 > div.left_7_3 > div > select');
+
+  // 高级检索2
+  await page.click('#_view_1545034775000 > div > div.search-wrapper.clearfix > div.advenced-search');
+
+  // 全文检索关键字
+  await page.type('#qbValue', '');
+  for (let index = 0; index < searchText.length; index++) {
+    await page.keyboard.press('Backspace');
+  }
+
+  await page.type('#qbValue', '处罚');
+
+  // 全文检索类型
+  await page.click('#qbType');
+  // 事实
+  await page.click('#qwTypeUl > li:nth-child(5)');
+
+
   // 检索
   await page.click('#searchBtn');
   await page.waitForSelector('#_view_1545184311000 > div.left_7_3 > div > select');
@@ -44,7 +73,7 @@ const puppeteer = require('puppeteer');
   let pageNum = 1;
   const data = [['案号', '标题', '案件类型', '当事人', '案由', 'pdf内容']];
   let i = 1;
-  while (pageNum < 40) {
+  while (pageNum < 6) {
     pageNum++;
     const view = await page.$('#_view_1545184311000');
     const lists = await view.$$('.LM_list');
@@ -68,9 +97,10 @@ const puppeteer = require('puppeteer');
         let content = await page2.$('#_view_1541573883000 > div > div.PDF_box');
         content = content !== null ? await content.evaluate(node => node.innerText) : '';
         data.push([ah, title, ajlx, client, reason, content]);
-        console.log(i++);
+        console.log(`${i++}:${ah}`);
         await page2.close();
       } catch (error) {
+        console.log(i++);
         console.error('error:', error);
         continue;
       }
@@ -91,4 +121,4 @@ const puppeteer = require('puppeteer');
     }
   ]);
   fs.writeFileSync('文书.xlsx', buffer, { 'flag': 'w' });
-}) ();
+})();
